ecd860f333c3eccb6cfec68f6570431a0d9f874f
Oleg Chorpita <chorpita@gmail.com>
2016-09-06 18:26:26 +0300

    start impl scrolling

--- js/app/scroll.js
+++ js/app/scroll.js
@@ -2,18 +2,17 @@
  * Created by i4790k on 06.09.2016.
  */
 
-$(function() {
-    $('a[href^="#"]').on('click', function(event) {
-
+$(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        debugger;
         var target = $(this.getAttribute('href'));
-
-        if( target.length ) {
+        target.
+        if (target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
             }, 450);
         }
-
     });
 
 });
